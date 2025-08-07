d334505cf88f2c123624f0a12e2700c96fb5cf32
harsh-gymble <harsh.gymble@gmail.com>
2025-08-07 12:27:57 +0530

    fix: cors issue resolved

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://3.110.167.205:5000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
